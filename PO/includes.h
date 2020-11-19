96a25674f8a81e4cf535fb0d1021c3a871974296
Mathieu CORMIER <mathieu.cormier@viacesi.fr>
2020-11-19 15:38:12 +0100

    version 1

--- PO/includes.h
+++ PO/includes.h
@@ -1,14 +1,16 @@
-// Déclaration des dépendances externes
-#ifndef PCH_H
-#define PCH_H
+#ifndef includes_H
+#define includes_H
 
-#include "Personnage.h"
 #include <iostream>
 #include <iterator> // Textes simplifié
 #include <string> // Obligatoire pour pouvoir utiliser les objets string
 #include <Windows.h> // sleep
+#include <fstream>
 
-//using namespace System;
+#include "Arme.h"
+#include "Personnage.h"
+
 using namespace std; // std::cout
 
 #endif
+
