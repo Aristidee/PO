0f9f6254d061a1f8fb6ae65a9ef772d966bcc6a2
AlexandreT33 <alexandre.tellier2@gmail.com>
2020-12-02 14:18:57 +0100

    reglage bug formé

--- PO/Client.h
+++ PO/Client.h
@@ -8,11 +8,14 @@ public:
 	Client();
 	~Client();
 	void ModifierClient();
+	void Ajouter_achat(int nbachat) { Nombre_d_achat += nbachat; }
+	void Retirer_achat(int nbmachat) { Nombre_d_achat -= nbmachat; }
 	void AfficherClient();
 
 private:
 	int IDClient;
+	int Numero_de_client;
 	struct ADRESSE adresse;
-	int NombreAchat;
+	int Nombre_d_achat;
 
 };
